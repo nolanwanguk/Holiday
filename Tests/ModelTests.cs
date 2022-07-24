f9dba43a626f99eccb8f0a7b9fb0bf9151008b72
nolanwanguk <nolan.w.sf@gmail.com>
2022-07-24 19:44:08 +0100

    add datetime conversion tests

---
+++ Tests/ModelTests.cs
@@ -0,0 +1,24 @@
+﻿using Holiday.Model;
+namespace Holiday.Tests;
+
+public class ModelTests
+{
+    [SetUp]
+    public void Setup()
+    { }
+
+    [Test]
+    public void Test_DateTime_Conversion_By_Slash()
+    {
+        string Case="2021/01/01";
+        QueryModel query = new QueryModel(DepartingFrom:"A",DepartureDate: Case,TravelingTo:"B",Duration:1);
+        Assert.That(query.DepartureDateConvert,Is.EqualTo(new DateTime(2021,1,1)));
+    }
+    [Test]
+    public void Test_DateTime_Conversion_By_Minus()
+    {
+        string Case="2021-01-01";
+        FlightModel flight = new FlightModel(airline: "A", from: "B", to: "C", id: 1, price: 100, departure_date: Case);
+        Assert.That(flight.DepartureDate,Is.EqualTo(new DateTime(2021,1,1)));
+    }
+}
