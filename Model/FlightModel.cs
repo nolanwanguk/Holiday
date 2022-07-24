3ecd86633e7f816687f3b6ace8f670b54dc54345
nolanwanguk <nolan.w.sf@gmail.com>
2022-07-24 19:22:00 +0100

    add flight data model

---
+++ Model/FlightModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Holiday.Model;
+
+public readonly record struct FlightModel(int id, string airline, string from, string to, int price, string departure_date)
+{
+    internal readonly int Id = id;
+    internal readonly string AirLine = airline;
+    internal readonly string DepartingFrom = from;
+    internal readonly string TravelingTo = to;
+    internal readonly int Price = price;
+    internal readonly DateTime DepartureDate = DateTime.Parse(departure_date);
+}
