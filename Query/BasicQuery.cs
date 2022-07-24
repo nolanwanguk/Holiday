58e70d8997b5b3c9b3ef5d1d8f392d0ddfe763cc
nolanwanguk <nolan.w.sf@gmail.com>
2022-07-24 19:33:08 +0100

    add basic query abstract class

---
+++ Query/BasicQuery.cs
@@ -0,0 +1,18 @@
+﻿using Holiday.Model;
+
+namespace Holiday.Query;
+
+public abstract class BasicQuery
+{
+    public string DepartingFrom { get; set; }
+    public string TravelingTo { get; set; }
+    public DateTime DepartureDate { get; set; }
+    public int Duration { get; set; }
+    
+    public List<Result>? Results { get; set; }
+
+    public abstract void Q(QueryModel q);
+
+    public abstract void Query(List<FlightModel> flights, List<HotelModel> hotels);
+    public abstract List<Result> Sort(List<FlightModel> flights, List<HotelModel> hotels);
+}
