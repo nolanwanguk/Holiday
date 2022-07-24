e0c064fb73df1d3904fa91e7d9e6a4e05c784437
nolanwanguk <nolan.w.sf@gmail.com>
2022-07-24 19:17:51 +0100

    add flight file reader

---
+++ IO/FlightsReader.cs
@@ -0,0 +1,28 @@
+﻿using Holiday.Model;
+using Newtonsoft.Json;
+
+namespace Holiday.IO;
+
+public class FlightsReader:BasicReader
+{
+    public int Counts;
+
+    public List<FlightModel> Results = new List<FlightModel>();
+
+    public override void BeforeRead() { }
+    public override void Reader(string path)
+    {
+        BeforeRead();
+        using (StreamReader r = new StreamReader(path))
+        {
+            string json = r.ReadToEnd();
+            List<FlightModel> results = JsonConvert.DeserializeObject<List<FlightModel>>(json);
+            Results = results;
+            Counts = results.Count;
+        }
+        AfterRead();
+    }
+
+    public override void AfterRead() { }
+    
+}
