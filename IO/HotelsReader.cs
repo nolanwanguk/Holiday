a4766a8ed3d4be96bd8b81b5e433712428bb7b51
nolanwanguk <nolan.w.sf@gmail.com>
2022-07-24 19:18:44 +0100

    add hotel file reader

---
+++ IO/HotelsReader.cs
@@ -0,0 +1,28 @@
+﻿using Holiday.Model;
+using Newtonsoft.Json;
+
+namespace Holiday.IO;
+
+public class HotelsReader:BasicReader
+{
+    public new int Counts;
+
+    public new List<HotelModel> Results = new List<HotelModel>();
+
+    public override void BeforeRead() { }
+    public override void Reader(string path)
+    {
+        BeforeRead();
+        using (StreamReader r = new StreamReader(path))
+        {
+            string json = r.ReadToEnd();
+            List<HotelModel> results = JsonConvert.DeserializeObject<List<HotelModel>>(json);
+            Results = results;
+            Counts = results.Count;
+        }
+        AfterRead();
+    }
+
+    public override void AfterRead() { }
+    
+}
