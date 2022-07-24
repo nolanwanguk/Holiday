b5e11dcaa36024cb5bde9ea0a8a970fea6e75e1b
nolanwanguk <nolan.w.sf@gmail.com>
2022-07-24 19:34:25 +0100

    add an interface of constant structures above basic query

--- Query/BasicQuery.cs
+++ Query/BasicQuery.cs
@@ -2,7 +2,18 @@
 
 namespace Holiday.Query;
 
-public abstract class BasicQuery
+public interface IBasicQuery
+{
+    public string DepartingFrom { get; set; }
+    public string TravelingTo { get; set; }
+    public DateTime DepartureDate { get; set; }
+    public int Duration { get; set; }
+    public List<Result>? Results { get; set; }
+    public void Q(QueryModel q);
+    public void Query(List<FlightModel> flights, List<HotelModel> hotels);
+}
+
+public abstract class BasicQuery:IBasicQuery
 {
     public string DepartingFrom { get; set; }
     public string TravelingTo { get; set; }
