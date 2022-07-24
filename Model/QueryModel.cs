d9def3964f493fedc0036b6e5fdd80816bdc9dd7
nolanwanguk <nolan.w.sf@gmail.com>
2022-07-24 19:23:36 +0100

    add query data model

---
+++ Model/QueryModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Holiday.Model;
+
+public record struct QueryModel(string DepartingFrom,string TravelingTo,string DepartureDate,int Duration)
+{
+    internal DateTime DepartureDateConvert { get; init; } = DateTime.Parse(DepartureDate);
+}
