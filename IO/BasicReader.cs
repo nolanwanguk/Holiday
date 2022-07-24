a1e0d0cb088c2f4524fc7f8c755efaf7d5770a4d
nolanwanguk <nolan.w.sf@gmail.com>
2022-07-24 19:30:38 +0100

    separate constant variables from abstract class to an interface

---
+++ IO/BasicReader.cs
@@ -0,0 +1,18 @@
+﻿namespace Holiday.IO;
+
+public interface IBasicReader
+{
+    public List<object>? Results { get; set; }
+    public int Counts { get; set; }
+    public void Read(string? path);
+}
+public abstract class BasicReader:IBasicReader
+{
+    
+    public List<object>? Results { get; set; }
+    public int Counts { get; set; }
+    public abstract void Read(string path);
+    public abstract void BeforeRead();
+    public abstract void Reader(string path);
+    public abstract void AfterRead();
+}
