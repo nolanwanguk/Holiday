a77cc5d56bf3d2b611a4ab5b14236636f98a7fc8
nolanwanguk <nolan.w.sf@gmail.com>
2022-07-24 23:29:59 +0100

    update IO tests conditions

--- Tests/IOTests.cs
+++ Tests/IOTests.cs
@@ -14,13 +14,15 @@ public class IOTests
     {
         var reader = new FlightsReader();
         reader.Read(@"C:\Users\nolan\RiderProjects\Holiday\Data\Flights.json");
-        Assert.That(reader.Counts,Is.GreaterThan(0));
+        Assert.That(reader.Counts,Is.EqualTo(12));
+        Assert.That(reader.Results.First().Id,Is.EqualTo(1));
     }
     [Test]
     public void Test_Hotel_File_Path()
     {
         var reader = new HotelsReader();
         reader.Read(@"C:\Users\nolan\RiderProjects\Holiday\Data\Hotels.json");
-        Assert.That(reader.Counts,Is.GreaterThan(0));
+        Assert.That(reader.Counts,Is.EqualTo(13));
+        Assert.That(reader.Results.First().Id,Is.EqualTo(1));
     }
 }
