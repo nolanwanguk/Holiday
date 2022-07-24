f9db62549c21e6411ad6943f6f7a1ad412b1dd44
nolanwanguk <nolan.w.sf@gmail.com>
2022-07-24 19:41:56 +0100

    separate common search conditions to BasicQuery class

--- Query/BasicQuery.cs
+++ Query/BasicQuery.cs
@@ -22,8 +22,25 @@ public abstract class BasicQuery:IBasicQuery
     
     public List<Result>? Results { get; set; }
 
-    public abstract void Q(QueryModel q);
+    public void Q(QueryModel q)
+    {
+        DepartingFrom = q.DepartingFrom;
+        TravelingTo = q.TravelingTo;
+        DepartureDate = q.DepartureDateConvert;
+        Duration = q.Duration;
+    }
 
-    public abstract void Query(List<FlightModel> flights, List<HotelModel> hotels);
+    public void Query(List<FlightModel> flights, List<HotelModel> hotels)
+    {
+        List<FlightModel> flight = flights.FindAll(i =>
+            i.DepartingFrom == DepartingFrom && i.TravelingTo == this.TravelingTo &&
+            i.DepartureDate == this.DepartureDate);
+        List<HotelModel> hotel = hotels.FindAll(i =>
+            i.ArrvialDate == this.DepartureDate && i.LocalAirports.Contains(this.TravelingTo) &&
+            i.Nights >= this.Duration);
+        
+        this.Results = Sort(flight, hotel);
+    }
+
     public abstract List<Result> Sort(List<FlightModel> flights, List<HotelModel> hotels);
 }
--- Query/LowestTotalPriceQuery.cs
+++ Query/LowestTotalPriceQuery.cs
@@ -4,26 +4,6 @@ namespace Holiday.Query;
 
 public class LowestTotalPriceQuery:BasicQuery
 {
-    public List<Result>? Results { get; set; }
-    
-    public override void Q(QueryModel q)
-    {
-        DepartingFrom = q.DepartingFrom;
-        TravelingTo = q.TravelingTo;
-        DepartureDate = q.DepartureDateConvert;
-        Duration = q.Duration;
-    }
-    public override void Query(List<FlightModel> flights, List<HotelModel> hotels)
-    {
-        List<FlightModel> flight = flights.FindAll(i =>
-            i.DepartingFrom == DepartingFrom && i.TravelingTo == this.TravelingTo &&
-            i.DepartureDate == this.DepartureDate);
-        List<HotelModel> hotel = hotels.FindAll(i =>
-            i.ArrvialDate == this.DepartureDate && i.LocalAirports.Contains(this.TravelingTo) &&
-            i.Nights >= this.Duration);
-        
-        this.Results = Sort(flight, hotel);
-    }
     public override List<Result> Sort(List<FlightModel> flights, List<HotelModel> hotels)
     {
         List<Result> Results = new List<Result>();
